8388ca5d75c3681532e70ffcf7c409750b5311d6
luckyharryji <xiangyu.ji@outlook.com>
2016-02-03 15:54:28 -0800

    finish dfs

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -61,11 +61,87 @@ impl NewGraph{
 
         NewGraph{vertices:vertex,vertive_num:length,adj_matrix:matrix}
     }
+
+
+    // return the neighbor node  of index
+    fn find_first_neighbor(&self, index:usize)->Option<usize>{
+        if index >(self.vertive_num-1){
+            return None;
+        }
+        for i in 0..self.vertive_num{
+            if self.adj_matrix[index][i] == 1{
+                return Some(i);
+            }
+        }
+        return None;
+
+    }
+
+    // return the next neighbor that cound potentionaly not been visited
+    fn find_next_neighbor(&self, index:usize,previous:usize)->Option<usize>{
+        if index >(self.vertive_num-1)||previous>(self.vertive_num-1) {
+            return None;
+        }
+        for i in previous+1..self.vertive_num{
+            if self.adj_matrix[index][i] == 1{
+                return Some(i);
+            }
+        }
+        return None;        
+    }
+
+
+    fn dfs_find(&self,node_index:usize,end_index:usize,visited:&mut Vec<usize>,depth_temp: usize,path:&mut Vec<usize>){
+        visited[node_index] = 1;
+        if depth_temp>=path.len(){
+            path.push(node_index);
+        }else{
+            path[depth_temp] = node_index;
+        }
+        let depth = depth_temp + 1;
+        let mut get_first_neighbor = self.find_first_neighbor(node_index);
+        while let Some(k) = get_first_neighbor{
+            if k == end_index{
+                path.push(k);
+                println!("out put one result");
+                for i in 0..depth+1{
+                    print!("{} {} {}\n", i, self.vertices[path[i]],path[i]);
+                }
+                print!("\n");
+                return   // how to fully jump out the recursion here?
+            }
+            if visited[k]==0{
+                self.dfs_find(k,end_index,visited,depth,path);
+            }
+            get_first_neighbor = self.find_next_neighbor(node_index,k);
+        }
+    }
+
+    fn dfs_path(&self, start:String, end:String){
+        let mut visited = Vec::<usize>::new();
+        let mut path = Vec::<usize>::new();
+        for i in 0..self.vertive_num{
+            visited.push(0);
+        }
+        let start_index = match char_to_index(&self.vertices,start){
+            Some(index) => index,
+            None => panic!("Input Start Point invalid"),
+        };
+
+        let end_index = match char_to_index(&self.vertices,end){
+            Some(index) => index,
+            None => panic!("Input End Point invalid"),
+        };
+        path.push(start_index);
+        let depth = 0;
+        self.dfs_find(start_index,end_index, &mut visited,depth,&mut path);
+    }
 }
 
 fn main() {
     let graph = get_graph();
     let new_graph = NewGraph::new(graph);
+    new_graph.dfs_path("a".to_owned(),"c".to_owned());
 }
 
 
